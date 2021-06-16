500bd16be4a7ef266292cac151c6f35defdce4e7
Roman Acevedo <roman.acevedo62@gmail.com>
2021-06-16 09:56:53 +0200

    update message

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 exports.helloHttp = (req, res) => {
-  let message = req.query.message || req.body.message || 'Hello World http!';
+  let message = req.query.message || req.body.message || 'Hello World http !!';
   res.status(200).send(message);
 };
